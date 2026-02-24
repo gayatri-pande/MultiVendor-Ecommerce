b6f8c932731a4e6adfe669f9d5774506dc9cc52e
Gayatri Pande <gayatripande099@gmail.com>
2026-02-24 22:10:12 +0530

    admin created

--- ecommerce/src/main/java/com/multivendor/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/multivendor/ecommerce/EcommerceApplication.java
@@ -1,7 +1,14 @@
 package com.multivendor.ecommerce;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.multivendor.ecommerce.entity.User;
+import com.multivendor.ecommerce.enums.Role;
+import com.multivendor.ecommerce.repository.UserRepository;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -10,4 +17,26 @@ public class EcommerceApplication {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
+	
+	@Bean
+	CommandLineRunner createAdmin(UserRepository userRepository,
+	                              PasswordEncoder passwordEncoder) {
+	    return args -> {
+
+	        if (userRepository.findByEmail("admin@multivendor.com").isEmpty()) {
+
+	            User admin = new User();
+	            admin.setName("Admin");
+	            admin.setEmail("admin@multivendor.com");
+	            admin.setPassword(passwordEncoder.encode("admin123"));
+	            admin.setRole(Role.ADMIN);
+
+	            userRepository.save(admin);
+
+	            System.out.println("Admin user created");
+	        }
+	    };
+	}
+
 }
+
