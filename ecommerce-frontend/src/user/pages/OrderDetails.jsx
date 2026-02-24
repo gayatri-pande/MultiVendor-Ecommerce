294f1052b0813e95da15f748a6d53ec3c84032e9
Gayatri Pande <gayatripande099@gmail.com>
2026-02-24 20:09:57 +0530

    fix css path issues

--- ecommerce-frontend/src/user/pages/OrderDetails.jsx
+++ ecommerce-frontend/src/user/pages/OrderDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../services/api";
-import "../styles/orderDetails.css";
+import "../styles/orderdetails.css";
 
 const OrderDetails = () => {
   const { id } = useParams();
