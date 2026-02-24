8ff5a09d76b3309bf8c626eec70bae9e9c0c8a29
Gayatri Pande <gayatripande099@gmail.com>
2026-02-24 19:45:01 +0530

    backend deployed domain added

--- ecommerce/src/main/java/com/multivendor/ecommerce/security/SecurityConfig.java
+++ ecommerce/src/main/java/com/multivendor/ecommerce/security/SecurityConfig.java
@@ -83,7 +83,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+        	    "https://your-frontend-domain.up.railway.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
