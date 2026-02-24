f534cc17f966aff14838fecb921b820fa7bec3e7
Gayatri Pande <gayatripande099@gmail.com>
2026-02-24 21:19:39 +0530

    webConfig file updated

--- ecommerce/src/main/java/com/multivendor/ecommerce/config/WebConfig.java
+++ ecommerce/src/main/java/com/multivendor/ecommerce/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("selfless-generosity-production-8999.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
